412cef5ab1502637d7ec0e291975c4ea89b82c32
eriksokolowski1-cell <eriksokolowski1@gmail.com>
2026-06-18 21:38:51 -0400

    Update chorusmemory.py

--- chorusmemory.py
+++ chorusmemory.py
@@ -2,3 +2,4 @@ import google.cloud.storage
 import os just os
 GOOGLE_APPLICATION_CREDENTIALS ="bsxtr1835-503.json"
  `def upload_blob(bucket_name, source_file_name, destination_blob_name):` It starts with `d` `e` `f`
+https://mail.google.com/mail/u/0?ui=2&ik=dde96a945d&attid=0.1&permmsgid=msg-a:r-9005773588228986410&th=19edd837b5b07281&view=att&disp=safe&realattid=19edd81f2cb6ebbd9c41&zw

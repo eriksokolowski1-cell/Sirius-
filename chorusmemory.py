144f4031df9897872e5707c2fb36858228fdf2d0
eriksokolowski1-cell <eriksokolowski1@gmail.com>
2026-06-18 22:06:08 -0400

    Update chorusmemory.py

--- chorusmemory.py
+++ chorusmemory.py
@@ -3,3 +3,16 @@ import os just os
 GOOGLE_APPLICATION_CREDENTIALS ="bsxtr1835-503.json"
  `def upload_blob(bucket_name, source_file_name, destination_blob_name):` It starts with `d` `e` `f`
 https://mail.google.com/mail/u/0?ui=2&ik=dde96a945d&attid=0.1&permmsgid=msg-a:r-9005773588228986410&th=19edd837b5b07281&view=att&disp=safe&realattid=19edd81f2cb6ebbd9c41&zw
+{
+  "type": "service_account",
+  "project_id": "studio-1075039541-b93a6",
+  "private_key_id": "f14df47b1347b6f3cf0f5874b456a6d34e165b4d",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCrwhHV/W6HS01w\n1J5XnK5en9206SWCEahoUF++bWfrgXxzgg3M4PmMr/vLuKauNMjHuoGPQJDj2pzs\nhieqqMFp+ji+/Qc3zp5bSVbeTro4S5mR/AexfF7xKr4kzcBbG2ruCA+FgvftidnV\nfgd9uz6SbDmV5JHcad/7JIagjLDYfJ/AW698iKPz9+RBi2rA+HQaxlSymC47QSMQ\nn9v/ms9Wq98NEnD0ue6D53J/YIFqNVh4AhX1+2OEvFodABsG3oTsi1g6tsg/BcRQ\n2iSFr2syfmj6bgGZomhO2pcwxpMUOBLYSQ5heLNhnZPF/h4UrvngFGshmoXGJ44v\n+bx9Q65NAgMBAAECggEAEMp4oZGp0qp5vXGJBz9HIUisomkhJGg4gqDKM1y7tN9k\nPLXhRn7/Nmc6jOMWJwqm+YuR6zYLKWiaQFnUb+UZKhSZo+Yq5KRGK94YEsSMhOtd\nwyfp0p/kzNvPrzEOwHlbiJNGJ9hdXMibVePWX07E97MtJVUCg2iK9SDQKSAykP84\n7czWaoROfOjCWC/VRakB5dgaA2gyqf7PC46RhsbUZfiW2kgrpngRLPch18sQ3FzO\ngMFyG743sX8SUuGMUKTOA6RD9vLkrcwJGX72Taog7Vi9Bx1tGQ+GCIpkP6w43jMM\nDclFj0aaLApJtx/UsbL/hqJjrGlMwSUQmKOUhwRwKwKBgQDUBx7UPUoT4cuQYJBm\n+hNPMyRwiOc7rHkyF5TOIqo/hGfYB7+4VMwo7nh3lok1oBXjKFTlDOGjji/sQAn5\n9rC3z6HmLitrGYyTvetKTY9yn7bVgDxKnjtVYi/7qAhxaTmS7XFLhpusRyYQPBm2\n+wG4J5AB2lGhvqgUgMAZT/Tq/wKBgQDPYPfNMBkrT1g5VgPfpHejhbC4m+hP7HSW\ns4UAKD67ORVHGPfH76X4J7R8pbdglxc2YfIcDUuqcABpmlgoGfGV+wlwv5y6FFzz\nYMcF4YG6x8NYesFkkhBGszDYkrThOEsYLL8AG430SoYYPk3sixTS5NA2Gd/Q+MtR\na1gJzbKiswKBgCynGz0Dc1hgkyhizcaCIBLr6F6y1k/g8N01zibjcgtSbTQ33Y6K\nfTyZjFFTKK5cen+OfOKIJfT1+u28Rt2XsHvlhGkfjBFQHiT3OFg7oVw4ep7YFNp6\nHHDM7uIG71BDItbonpWTUbGkgkWlAp1gm2oXGuLpAYwYxPkcbFhoWGItAoGBAM3J\ngd3B3xgVUo0DCDEDuh0w618mh2/YSxHI7zBqv2V2jeDRrABhlacdb4NJxyE4jbZP\nl7uyjeWB40bn/Jc+t3eHd6TE4DffQSXaYWU6lEvPM9A3cHXmY/RkmRIw8wGTpSFG\nNftbXJCQ62RfX2Bd27oziVJGds2eNdSD2wdPcfb1AoGABPgy/7gu7u4ycjniobCO\npzZ6egxzMHKt9MtxQpLtFicsTxSTAsl6xgscGkDKCk+U6ZSXSjKdqZe2KVE7CaFL\n9a2u65x9BHA4LxgIj8roNyliuPaD9mstEMnxfb0QX+jfY+whIOClePrv1sypQGk3\n+SQMSDBOYpEr2wjRXtVCBUY=\n-----END PRIVATE KEY-----\n",
+  "client_email": "chorusv1@studio-1075039541-b93a6.iam.gserviceaccount.com",
+  "client_id": "112581575125643328134",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/chorusv1%40studio-1075039541-b93a6.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
